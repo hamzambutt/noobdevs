aed2b5053849a6767ac114a4057ed1bb53ae0c25
hamzambutt <hamzamasood420@gmail.com>
2026-08-19 00:30:44 +0500

    Update gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OQQyDJfOy7l1/ujfGVejg0cu9yuiYJF/Ls8/+P6SHvw=\"\n}"
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0cbk-n2._.css","static/chunks/node_modules_0kzre6z._.js","static/chunks/app_components_SmoothScroll_tsx_0iyak0q._.js","static/chunks/app_1b3sv1o._.js"],
-    source: "entry"
-});
